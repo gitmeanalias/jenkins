679bbe1941db3a4c330991a5bf844b67abe303c3
gitmeanalias <78276143+gitmeanalias@users.noreply.github.com>
2021-04-12 03:30:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send("<div style="height: '100vh';background: 'blue';"><h1 style="color: orange">Welcome guys, I say hello!! from Jenkins</h1></div>")
+    res.send("<h1 style="color: orange">Welcome guys, I say hello!! from Jenkins</h1>")
 });
 
 app.get('/name/:myname', (req, res) => {
